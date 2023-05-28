9e3b40292b98e57bdc49207ad5d42e19584d43a5
AdelekeOfAfrica <adelekeofafrica@gmail.com>
2023-05-28 20:37:56 +0100

    worked on the our works page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Header from './components/Header.vue'
 import Hero from './Hero.vue'
+import Works from './Works.vue'
 </script>
 
 <template>
 
   <Header />
   <Hero />
+  <Works />
 </template>
 
 <style scoped>
